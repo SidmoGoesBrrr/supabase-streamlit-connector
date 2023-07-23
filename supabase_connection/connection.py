249bb54fe31036f331781c23ee1fb1ab876735e8
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-07-23 22:30:29 +0530

    Update connection.py

--- supabase_connection/connection.py
+++ supabase_connection/connection.py
@@ -15,8 +15,7 @@ class SUPABASECONNECTION(ExperimentalBaseConnection[supabase.Client]):
         key = st.secrets["SUPABASE_KEY"]
         return supabase.create_client(url, key)
     
-    @cache_data(ttl=3600)
-    def _select(self, table,function, ttl: int = 3600, **kwargs):
+    def select(self, table,function, ttl: int = 3600, **kwargs):
     
         client = self._connect()
         #result=client.table(table).select(function).execute()
@@ -26,22 +25,19 @@ class SUPABASECONNECTION(ExperimentalBaseConnection[supabase.Client]):
         result=client.table(table).select(function).execute()
         return result
     
-    @cache_data(ttl=3600)
-    def _select_filter(self, table,function, column,operator,criteria, ttl: int = 3600, **kwargs):
+    def select_filter(self, table,function, column,operator,criteria, ttl: int = 3600, **kwargs):
         client = self._connect()
         if isinstance(function, list):
             function = ', '.join(function)
         result=client.table(table).select(function).filter(column=column,operator=operator,criteria=criteria).execute()
         return result
     
-    @cache_data(ttl=3600)
-    def _insert(self, table, data, ttl: int = 3600, **kwargs):
+    def insert(self, table, data, ttl: int = 3600, **kwargs):
         client = self._connect()
         result=client.table(table).insert(data).execute()
         return result
     
-    @cache_data(ttl=3600)
-    def _update(self, table, data,row_id ,ttl: int = 3600, **kwargs):
+    def update(self, table, data,row_id ,ttl: int = 3600, **kwargs):
         client = self._connect()
         result=client.table(table).update(data).eq("id",row_id).execute()
         return result
